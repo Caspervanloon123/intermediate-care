589af4b99696e3cdcf66926536c151a9bcc34e50
Caspervanloon123 <122274177+Caspervanloon123@users.noreply.github.com>
2024-06-03 17:11:01 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,9 +11,22 @@ num_locations = st.number_input("Voer het aantal locaties in", min_value=1, step
 # Checkbox voor het aangeven van prioriteit
 prioriteit = st.checkbox("Prioriteit")
 
-# Toon het ingevoerde aantal locaties en de prioriteit
-st.write("Aantal locaties:", num_locations)
-if prioriteit:
-    st.write("Prioriteit: Ja")
-else:
-    st.write("Prioriteit: Nee")
+# Functie om de invoervelden voor aankomsten te tonen
+def show_arrival_inputs():
+    st.subheader("Aankomsten GRZ:")
+    arrival_GRZ = st.number_input("Arrival GRZ per dag", value=0.54)
+
+    st.subheader("Aankomsten High Complex:")
+    arrival_HOS_High = st.number_input("Arrival HOS High per dag", value=0.94)
+    arrival_EMD = st.number_input("Arrival EMD per dag", value=0.83)
+    arrival_GPR_High = st.number_input("Arrival GPR High per dag", value=1.34)
+
+    st.subheader("Aankomsten Low Complex:")
+    arrival_GPR_Low = st.number_input("Arrival GPR Low per dag", value=1.91)
+
+# Titel van de applicatie
+st.title("STRC Waiting Time Calculator")
+
+# Knop om de aankomsten te tonen
+if st.button("Aankomsten"):
+    show_arrival_inputs()
