37221d0c8e3b787aca49040dfd33b8b97c6ce8b8
Caspervanloon123 <122274177+Caspervanloon123@users.noreply.github.com>
2024-06-03 17:12:46 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,30 +11,25 @@ num_locations = st.number_input("Voer het aantal locaties in", min_value=1, step
 # Checkbox voor het aangeven van prioriteit
 prioriteit = st.checkbox("Prioriteit")
 
-def show_or_hide_arrival_inputs(flag):
-    if flag:
-        st.subheader("Aankomsten GRZ:")
-        arrival_GRZ = st.number_input("Arrival GRZ per dag", value=0.54)
+# Function to show the arrival inputs
+def show_arrival_inputs():
+    st.subheader("Aankomsten GRZ:")
+    arrival_GRZ = st.number_input("Arrival GRZ per dag", value=0.54)
 
-        st.subheader("Aankomsten High Complex:")
-        arrival_HOS_High = st.number_input("Arrival HOS High per dag", value=0.94)
-        arrival_EMD = st.number_input("Arrival EMD per dag", value=0.83)
-        arrival_GPR_High = st.number_input("Arrival GPR High per dag", value=1.34)
+    st.subheader("Aankomsten High Complex:")
+    arrival_HOS_High = st.number_input("Arrival HOS High per dag", value=0.94)
+    arrival_EMD = st.number_input("Arrival EMD per dag", value=0.83)
+    arrival_GPR_High = st.number_input("Arrival GPR High per dag", value=1.34)
 
-        st.subheader("Aankomsten Low Complex:")
-        arrival_GPR_Low = st.number_input("Arrival GPR Low per dag", value=1.91)
-    else:
-        st.write("")  # Empty placeholder to hide the inputs
+    st.subheader("Aankomsten Low Complex:")
+    arrival_GPR_Low = st.number_input("Arrival GPR Low per dag", value=1.91)
 
 # Title of the application
 st.title("STRC Waiting Time Calculator")
 
 # Flag to track the visibility of the arrival inputs
-show_inputs = False
+show_inputs = st.checkbox("Aankomsten")
 
-# Button to toggle the visibility of the arrival inputs
-if st.button("Aankomsten"):
-    show_inputs = not show_inputs  # Toggle the flag
-
-# Show or hide the arrival inputs based on the flag
-show_or_hide_arrival_inputs(show_inputs)
+# Show the arrival inputs if the checkbox is checked
+if show_inputs:
+    show_arrival_inputs()
