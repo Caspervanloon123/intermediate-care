c5602c9b1d270846a4888eb5c046b253885f314c
Caspervanloon123 <122274177+Caspervanloon123@users.noreply.github.com>
2024-06-03 17:06:03 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,16 @@ import pandas as pd
 # Titel van de applicatie
 st.title("STRC Waiting Time Calculator")
 
+
+# Input box voor het aantal locaties
+num_locations = st.number_input("Voer het aantal locaties in", min_value=1, step=1)
+
+# Checkbox voor het aangeven van prioriteit
+prioriteit = st.checkbox("Prioriteit")
+
+# Toon het ingevoerde aantal locaties en de prioriteit
+st.write("Aantal locaties:", num_locations)
+if prioriteit:
+    st.write("Prioriteit: Ja")
+else:
+    st.write("Prioriteit: Nee")
