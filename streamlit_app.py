eab9d915cf77ed86d5e213f38a91f294907b93ea
Caspervanloon123 <122274177+Caspervanloon123@users.noreply.github.com>
2024-06-03 18:08:16 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,69 @@ import pandas as pd
 
 # Titel van de applicatie
 st.title("STRC Waiting Time Calculator")
+# Function to display bed share inputs
+def display_bed_share_inputs(value, num_locations):
+    beds_input = []
+    high_complex_beds = []
+    high_complex_nurses = []
+    grz_beds = []
+    grz_nurses = []
+    shared_beds = []
+    elv_low_complex_beds = []
+    elv_low_complex_nurses = []
+    emergency_beds = []
+    elv_high_complex_beds = []
+    elv_high_complex_nurses = []
+    total_beds = []
+    total_nurses = []
+    trw_beds = []
 
+    if not num_locations or num_locations == 0:
+        return beds_input, [], [], [], [], [], [], [], [], [], [], [], [], []
+
+    for i in range(num_locations):
+        if value == 'none':
+            high_complex_beds.append(st.number_input(f'Location {i+1} _ High Complex Beds:', value=0, key=f'high_complex_beds_{i}'))
+            high_complex_nurses.append(st.number_input(f'Location {i+1} _ High Complex Nurses:', value=0, key=f'high_complex_nurses_{i}'))
+            grz_beds.append(st.number_input(f'Location {i+1} _ GRZ Beds:', value=0, key=f'grz_beds_{i}'))
+            grz_nurses.append(st.number_input(f'Location {i+1} _ GRZ Nurses:', value=0, key=f'grz_nurses_{i}'))
+            shared_beds.append(st.number_input(f'Location {i+1} _ Shared Beds:', value=0, key=f'shared_beds_{i}'))
+            elv_low_complex_beds.append(st.number_input(f'Location {i+1} _ ELV Low Complex Beds:', value=0, key=f'elv_low_complex_beds_{i}'))
+            elv_low_complex_nurses.append(st.number_input(f'Location {i+1} _ ELV Low Complex Nurses:', value=0, key=f'elv_low_complex_nurses_{i}'))
+            emergency_beds.append(st.number_input(f'Location {i+1} _ Emergency Beds:', value=0, key=f'emergency_beds_{i}'))
+        elif value == 'full':
+            elv_high_complex_beds.append(st.number_input(f'Location {i+1} _ ELV High Complex Beds:', value=0, key=f'elv_high_complex_beds_{i}'))
+            elv_high_complex_nurses.append(st.number_input(f'Location {i+1} _ ELV High Complex Nurses:', value=0, key=f'elv_high_complex_nurses_{i}'))
+            elv_low_complex_beds.append(st.number_input(f'Location {i+1} _ ELV Low Complex Beds:', value=0, key=f'elv_low_complex_beds_{i}'))
+            elv_low_complex_nurses.append(st.number_input(f'Location {i+1} _ ELV Low Complex Nurses:', value=0, key=f'elv_low_complex_nurses_{i}'))
+            emergency_beds.append(st.number_input(f'Location {i+1} _ Emergency Beds:', value=0, key=f'emergency_beds_{i}'))
+        elif value == 'partial':
+            high_complex_beds.append(st.number_input(f'Location {i+1} _ High Complex Beds:', value=0, key=f'high_complex_beds_{i}'))
+            high_complex_nurses.append(st.number_input(f'Location {i+1} _ High Complex Nurses:', value=0, key=f'high_complex_nurses_{i}'))
+            grz_beds.append(st.number_input(f'Location {i+1} _ GRZ Beds:', value=0, key=f'grz_beds_{i}'))
+            grz_nurses.append(st.number_input(f'Location {i+1} _ GRZ Nurses:', value=0, key=f'grz_nurses_{i}'))
+            shared_beds.append(st.number_input(f'Location {i+1} _ Shared Beds:', value=0, key=f'shared_beds_{i}'))
+            elv_low_complex_beds.append(st.number_input(f'Location {i+1} _ ELV Low Complex Beds:', value=0, key=f'elv_low_complex_beds_{i}'))
+            elv_low_complex_nurses.append(st.number_input(f'Location {i+1} _ ELV Low Complex Nurses:', value=0, key=f'elv_low_complex_nurses_{i}'))
+            emergency_beds.append(st.number_input(f'Location {i+1} _ Emergency Beds:', value=0, key=f'emergency_beds_{i}'))
+        elif value == 'total':
+            total_beds.append(st.number_input(f'Location {i+1} _ Total Beds:', value=0, key=f'total_beds_{i}'))
+            total_nurses.append(st.number_input(f'Location {i+1} _ Total Nurses:', value=0, key=f'total_nurses_{i}'))
+            emergency_beds.append(st.number_input(f'Location {i+1} _ Emergency Beds:', value=0, key=f'emergency_beds_{i}'))
+        elif value == 'trw':
+            high_complex_beds.append(st.number_input(f'Location {i+1} _ High Complex Beds:', value=0, key=f'high_complex_beds_{i}'))
+            high_complex_nurses.append(st.number_input(f'Location {i+1} _ High Complex Nurses:', value=0, key=f'high_complex_nurses_{i}'))
+            grz_beds.append(st.number_input(f'Location {i+1} _ GRZ Beds:', value=0, key=f'grz_beds_{i}'))
+            grz_nurses.append(st.number_input(f'Location {i+1} _ GRZ Nurses:', value=0, key=f'grz_nurses_{i}'))
+            trw_beds.append(st.number_input(f'Location {i+1} _ TRW Beds:', value=0, key=f'trw_beds_{i}'))
+            elv_low_complex_beds.append(st.number_input(f'Location {i+1} _ ELV Low Complex Beds:', value=0, key=f'elv_low_complex_beds_{i}'))
+            elv_low_complex_nurses.append(st.number_input(f'Location {i+1} _ ELV Low Complex Nurses:', value=0, key=f'elv_low_complex_nurses_{i}'))
+            emergency_beds.append(st.number_input(f'Location {i+1} _ Emergency Beds:', value=0, key=f'emergency_beds_{i}'))
+        # Add conditions for other bed share types if any
+
+    return (beds_input, high_complex_beds, high_complex_nurses, grz_beds, grz_nurses, shared_beds,
+            elv_low_complex_beds, elv_low_complex_nurses, emergency_beds, elv_high_complex_beds,
+            elv_high_complex_nurses, total_beds, total_nurses, trw_beds)
 # List of scenario variables
 scenario_vars = [
     "Scen_shared_beds_Full", "Scen_NO_Sharing", "Scen_Part_bed_share", "Scen_Triage_ward", "Scen_Total_Sharing"
