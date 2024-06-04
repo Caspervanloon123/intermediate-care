f0ac71851deffb2ed4262319dc187501a965ab32
Caspervanloon123 <122274177+Caspervanloon123@users.noreply.github.com>
2024-06-04 19:42:03 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6220,11 +6220,17 @@ if st.button('Start Simulation'):
     table2 = pd.DataFrame(output_df[table2_columns])
     table3 = pd.DataFrame(output_df[table3_columns])
     table4 = pd.DataFrame(output_df[table4_columns])
+    table2['Perc_with_HOSP_adm'] = table2['Perc_with_HOSP_adm']*100
+    table2['Perc_with_HOSP_adm_HOSP'] = table2['Perc_with_HOSP_adm_HOSP']*100
+    table3['serv_level'] = table3['serv_level']*100
+    table3['bez_gr_total'] = table3['bez_gr_total']*100
+    table3['bez_gr_High'] = table3['bez_gr_High']*100
+    table3['bez_gr_Low'] = table3['bez_gr_Low']*100
     # Hernoem de kolommen
     table1.rename(columns = {'Wt_from_HOSP_GRZ':'Ziekenhuis GRZ', 'Wt_from_HOSP_High':'Ziekenhuis Hoog Complex', 'Wt_from_GPR_High':'Huisarts Hoog Complex', 'Wt_from_GPR_Low':'Huisarts Laag Complex', 'Wt_to_TRW':'Observatie', 'WT_from_EMD':'Spoedeisendehulp'},inplace = True)
-    table2.rename(columns = {'Perc_with_HOSP_adm':'Percentage met ziekenhuisopname vanaf spoedeisendehulp', 'Number with hosp adm EMD':'Aantal met ziekenhuisopname vanaf spoedeisendehulp', 'Perc_with_HOSP_adm_HOSP':'Percentage met ziekenhuisopname vanaf het ziekenhuis','Number with hosp adm HOSP':'Aantal met ziekenhuisopname vanaf het ziekenhuis', 'nr_pat_repl':'Aantal verplaatsingen'}, inplace = True)
-    table3.rename(columns = {'serv_level':'Service level', 'los_ELV_High':'Gemiddelde verplijfduur Hoog Complex', 'los_ELV_Low':'Gemiddelde verblijfduur Laag Complex', 'bez_gr_total':'Bezettingsgraad totaal',
-    'bez_gr_High':'Bezettingsgraad Hoog Complex', 'bez_gr_Low':'Bezettingsgraad Laag Complex'}, inplace =True)
+    table2.rename(columns = {'Perc_with_HOSP_adm':'Percentage met ziekenhuisopname vanaf spoedeisendehulp (%)', 'Number with hosp adm EMD':'Aantal met ziekenhuisopname vanaf spoedeisendehulp', 'Perc_with_HOSP_adm_HOSP':'Percentage met ziekenhuisopname vanaf het ziekenhuis (%)','Number with hosp adm HOSP':'Aantal met ziekenhuisopname vanaf het ziekenhuis', 'nr_pat_repl':'Aantal verplaatsingen'}, inplace = True)
+    table3.rename(columns = {'serv_level':'Service level (%)', 'los_ELV_High':'Gemiddelde verplijfduur Hoog Complex', 'los_ELV_Low':'Gemiddelde verblijfduur Laag Complex', 'bez_gr_total':'Bezettingsgraad totaal (%)',
+    'bez_gr_High':'Bezettingsgraad Hoog Complex (%)', 'bez_gr_Low':'Bezettingsgraad Laag Complex (%)'}, inplace =True)
     table4.rename(columns = {'Number of Locations ELV':'Aantal ELV Locaties', 'Number of beds ELV_High':'Aantal ELV Hoog Complex bedden', 'Number of beds ELV_Low':'Aantal ELV Laag Complex bedden',
     'Number of beds GRZ':'Aantal bedden Geriatrische Zorg', 'Number of beds High Complex':'Aantal Hoog Complexe bedden', 'Number of shared beds':'Aantal gedeelde bedden', 'Number of TRW beds':'Aantal bedden voor observatie', 'Number of beds ELV Total':'Aantal bedden ELV Totaal'},inplace =True)
     table1.index = ['Wachttijd (dagen)']
