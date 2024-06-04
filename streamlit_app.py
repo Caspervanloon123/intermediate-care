5c316b6d3283fe64a754f520b4b1e51431f09668
Caspervanloon123 <122274177+Caspervanloon123@users.noreply.github.com>
2024-06-04 18:09:11 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6215,8 +6215,8 @@ if st.button('Display DataFrame'):
     table4 = pd.DataFrame(output_df[table4_columns])
     
     #st.write(df1)
-    st.write(table1.T)
-    st.bar_chart(table1)
+    st.write(table1)
+    st.bar_chart(table1.T)
     st.write(table2)
     st.write(table3)
     st.write(table4)
