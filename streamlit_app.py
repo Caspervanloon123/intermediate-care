4be9f92f0823cdf2801be98fda43f436399b3716
Caspervanloon123 <122274177+Caspervanloon123@users.noreply.github.com>
2024-06-04 21:29:00 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ background-image: url("https://i.postimg.cc/4xgNnkfX/Untitled-design.png");
 background-size: cover;
 background-position: center center;
 background-repeat: no-repeat;
-background-attachment: local;
+background-attachment: local; 
 }}
 [data-testid="stHeader"] {{
 background: rgba(0,0,0,0);
